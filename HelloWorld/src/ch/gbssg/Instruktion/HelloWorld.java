3849ecf3ef4ea0b4395ced7c7d18e0443c8328fa
franjofranjic <franjo.franjic@edu.gbssg.ch>
2019-10-28 19:45:44 +0100

    Ready

---
+++ HelloWorld/src/ch/gbssg/Instruktion/HelloWorld.java
@@ -0,0 +1,8 @@
+package ch.gbssg.Instruktion;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		String yourname = "";
+		System.out.println("Hello World" + yourname);
+	}
+}
