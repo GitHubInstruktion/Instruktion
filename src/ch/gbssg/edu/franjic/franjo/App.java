16771a338cb83100083a0e905b47ab3ca65c466c
franjofranjic <franjo.franjic@edu.gbssg.ch>
2019-11-05 06:42:46 +0100

    personal files

--- src/ch/gbssg/edu/franjic/franjo/App.java
+++ src/ch/gbssg/edu/franjic/franjo/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String username = "Max Muster";
+		String username = "Franjo Franjic";
 		System.out.println("Hallo "+ username);
 	}
 
